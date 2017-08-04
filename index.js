6ab8d03a9f27e98ecdf729e7a0c062b18c9cbf6c
Frank Iriarte <jurrish@gmail.com>
2017-08-04 11:31:14 -0700

    dispatching

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 import categoryReducer from './reducer/boards.js'
-
+import { categoryCreate, categoryUpdate, categoryDelete, categoryReset } from './action/category-actions.js';
 //create a store
 
-const store = createStore
+const store = createStore(categoryReducer);
+
+console.log(store);
+console.log('state', store.getState()) //returns inital state we provide in the reducer
+
+//dispatch an action
+store.dispatch(categoryCreate({ title: 'cool beans'}) )
+store.getState() // this returns a single thing with a uuid and timestamp for the new coolbeans category
